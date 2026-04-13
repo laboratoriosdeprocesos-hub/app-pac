2684b596030489ba87ec1eb8b88bd34e198b0602
laboratoriosdeprocesos-hub <laboratoriosdeprocesos@servaf.com>
2026-04-13 15:53:10 -0500

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from sklearn.neighbors import NearestNeighbors
 # CONFIGURACION GENERAL
 # =========================================
 st.set_page_config(
-    page_title="PTAP - Recomendación PAC",
+    page_title="PTAP - Herramienta Web",
     page_icon="💧",
     layout="wide"
 )
